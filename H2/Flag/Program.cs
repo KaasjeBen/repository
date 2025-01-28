7f31db2cd09d16bd335616f188eb0f4e36809338
Ben <D310581@edu.curio.nl>
2025-01-28 14:27:08 +0100

    kjaas

---
+++ H2/Flag/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print de nederlandse vlag
+            Console.WriteLine("Nederlandse vlag:");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("█████████████");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("█████████████");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("█████████████");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Rusische vlag:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("█████████████");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("█████████████");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("█████████████");
+
+
+            // Wacht op key press
+            Console.ReadKey();
+        }
+    }
+}
